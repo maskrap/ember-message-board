817ee52649627c9020fb18071794f02133d5c694
Sam Park <sampark18@gmail.com>
2016-08-18 20:23:07 -0700

    Progress with getting answers to show up

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,20 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewQuestion: false,
-  actions: {
-    questionFormShow() {
-      this.set('addNewQuestion', true);
-    },
-
-    save() {
-      var params = {
-        questionTitleView: this.get('questionTitleView')? this.get('questionTitleView') : "",
-        questionAuthorView: this.get('questionAuthorView')? this.get('questionAuthorView') : "",
-        topic: this.get('topic')? this.get('topic') : "",
-      };
-      this.set('addNewQuestion', false);
-      this.sendAction('save', params);
-    }
-  }
+  
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+      return this.store.query('article', {
+        orderBy: 'timestamp',
+        limitToLast: 6
+        });
+    },
+  model() {
     return this.store.findAll('question');
   },
 
@@ -10,6 +16,16 @@ export default Ember.Route.extend({
     var newQuestion = this.store.createRecord('question', params);
     newQuestion.save();
     this.transitionTo('index');
+   },
+
+   update(question, params) {
+     Object.keys(params).forEach(function(key) {
+       if(params[key]!==undefined) {
+         question.set(key,params[key]);
+       }
+     });
+     question.save();
+     this.transitionTo('index');
    },
 
    destroyQuestion(question) {
