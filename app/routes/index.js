867e3a7821510896ac15c1d228533f89226f1a5a
Sam Park <sampark18@gmail.com>
2016-08-19 09:43:34 -0600

    Fixing spelling errors

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.query('article', {
+    return this.store.query('question', {
       orderBy: 'titleListView',
       });
   }
