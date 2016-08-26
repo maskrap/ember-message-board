6d6270cc1717e939a5e05f2ae9d03557e4090804
Sam Park <sampark18@gmail.com>
2016-08-26 08:41:02 -0700

    Add some styling adjustments and fix adding to favorites from question detail page

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
+  
   actions: {
     update(question, params) {
       this.sendAction('update', question, params);
-    }
+    },
+    addToFav(question) {
+      this.get('favoritesList').add(question);
+    },
   }
 });
