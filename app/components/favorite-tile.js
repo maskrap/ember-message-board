4fffff0d3d42b66b9b6bf8bd111cc6364284d50c
Sam Park <sampark18@gmail.com>
2016-08-26 13:27:22 -0700

    Add computed property of number of responses to question

--- app/components/favorite-tile.js
+++ app/components/favorite-tile.js
@@ -2,7 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  sortBy: ['author:asc'],
-  sortedQuestions: Ember.computed.sort('questions', 'sortBy'),
-
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service(),
 
+  sortedAnswers: Ember.computed('questions.answers', function() {
+    var answerCount = this.get('question.answers.length');
+    return answerCount;
+  }),
+
   actions: {
     addToFav(question) {
       this.get('favoritesList').add(question);
